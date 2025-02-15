bee83357872f901169ef644e8aba148cb3e2dd0f
truthixify <truthixify@gmail.com>
2025-02-15 22:16:47 +0100

    fully implemented merkle tree with history

--- programs/sukura/src/lib.rs
+++ programs/sukura/src/lib.rs
@@ -1,8 +1,2 @@
-// use anchor_lang::solana_program::poseidon;
-
-mod poseidon;
-mod verifier;
 mod merkle_tree;
-mod lol;
-extern crate ff;
-
+mod verifier;
--- utils/generate_verifier.js
+++ utils/generate_verifier.js
@@ -1,18 +1,18 @@
 import {
-    fullProve,
-    parseProofToBytesArray,
-    parseToBytesArray,
-} from './parse_proof_to_rust.js';
-import { parseVk } from './parse_vk_to_rust.js';
-import fs from 'fs';
+  fullProve,
+  parseProofToBytesArray,
+  parseToBytesArray,
+} from "./parse_proof_to_rust.js";
+import { parseVk } from "./parse_vk_to_rust.js";
+import fs from "fs";
 
 export async function generateRustVerifier() {
-    const wasmPath = process.argv[2];
-    const zkeyPath = process.argv[3];
-    const proofInputPath = process.argv[4];
-    const verifyingKeyInputPath = process.argv[5];
-    if (!wasmPath || !zkeyPath || !proofInputPath || !verifyingKeyInputPath) {
-        const err = `error: missing required arguments
+  const wasmPath = process.argv[2];
+  const zkeyPath = process.argv[3];
+  const proofInputPath = process.argv[4];
+  const verifyingKeyInputPath = process.argv[5];
+  if (!wasmPath || !zkeyPath || !proofInputPath || !verifyingKeyInputPath) {
+    const err = `error: missing required arguments
 
 Usage: 
   node ./utils/generate_verifier.js <wasm_file> <zkey_file> <input_json> <verification_key_json>
@@ -27,34 +27,31 @@ Example:
   node ./utils/generate_verifier.js ./hash.wasm ./circuit_final.zkey ./input.json ./verification_key.json
 
 For more information, try '--help'`;
-        throw new Error(err);
-    }
+    throw new Error(err);
+  }
 
-    const outputPath = process.argv[6]
-        ? `${process.argv[6]}/verifier.rs`
-        : 'verifier.rs';
+  const outputPath = process.argv[6]
+    ? `${process.argv[6]}/verifier.rs`
+    : "verifier.rs";
 
-    const proofInputFile = fs.readFileSync(proofInputPath, 'utf8');
-    const proofInputs = JSON.parse(proofInputFile);
+  const proofInputFile = fs.readFileSync(proofInputPath, "utf8");
+  const proofInputs = JSON.parse(proofInputFile);
 
-    const verifyingKeyInputFile = fs.readFileSync(
-        verifyingKeyInputPath,
-        'utf8'
-    );
-    const verifyingKeyJson = JSON.parse(verifyingKeyInputFile);
+  const verifyingKeyInputFile = fs.readFileSync(verifyingKeyInputPath, "utf8");
+  const verifyingKeyJson = JSON.parse(verifyingKeyInputFile);
 
-    // const { proof, publicSignals } = await fullProve(
-    //     proofInputs,
-    //     wasmPath,
-    //     zkeyPath
-    // );
+  // const { proof, publicSignals } = await fullProve(
+  //     proofInputs,
+  //     wasmPath,
+  //     zkeyPath
+  // );
 
-    // let proofArr = parseProofToBytesArray(proof);
-    // proofArr = [...proofArr.proofA, ...proofArr.proofB, ...proofArr.proofC];
-    // const publicSignalsArr = parseToBytesArray(publicSignals);
-    const verifyingKey = await parseVk(verifyingKeyJson);
+  // let proofArr = parseProofToBytesArray(proof);
+  // proofArr = [...proofArr.proofA, ...proofArr.proofB, ...proofArr.proofC];
+  // const publicSignalsArr = parseToBytesArray(publicSignals);
+  const verifyingKey = await parseVk(verifyingKeyJson);
 
-    const rustVerifier = `use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
+  const rustVerifier = `use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
 use groth16_solana::{groth16::{Groth16Verifier, Groth16Verifyingkey}, errors::Groth16Error};
 use std::ops::Neg;
 type G1 = ark_bn254::g1::G1Affine;
@@ -115,17 +112,17 @@ pub fn verify_proof(data: &[u8]) -> Result<bool, Groth16Error> {
 }
 `;
 
-    // const testRustProofAndPublicInput = `const PROOF: [u8; 256] = [${proofArr}];
+  // const testRustProofAndPublicInput = `const PROOF: [u8; 256] = [${proofArr}];
 
-// const PUBLIC_SIGNALS: [[u8; 32]; 1] = [[${publicSignalsArr}]];`;
+  // const PUBLIC_SIGNALS: [[u8; 32]; 1] = [[${publicSignalsArr}]];`;
 
-    fs.writeFileSync(outputPath, rustVerifier);
-    // fs.writeFileSync('proof.rs', testRustProofAndPublicInput);
-    console.log(
-        '✅ Rust verifier written to',
-        outputPath,
-        'and test rust proof and public input written to proof.rs'
-    );
+  fs.writeFileSync(outputPath, rustVerifier);
+  // fs.writeFileSync('proof.rs', testRustProofAndPublicInput);
+  console.log(
+    "✅ Rust verifier written to",
+    outputPath,
+    "and test rust proof and public input written to proof.rs"
+  );
 }
 
 generateRustVerifier();
